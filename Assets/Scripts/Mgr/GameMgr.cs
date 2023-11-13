ae5a0c53205292b4c54fd53b60fe1f7800a8244f
alone-snow <1768927122@qq.com>
2023-11-13 16:55:44 +0800

    11111

--- Assets/Scripts/Mgr/GameMgr.cs
+++ Assets/Scripts/Mgr/GameMgr.cs
@@ -54,12 +54,39 @@ public class GameMgr : BaseManager<GameMgr>
     /// </summary>
     public void CreatRoom()
     {
+        UIManager.Instance.ShowPanel<TipPanel>((o) => { o.SetCurrent("房间创建中"); });
         (MyNetworkManager.singleton as MyNetworkManager).CreatRoom((o) => 
         {
             if (o.m_eResult != EResult.k_EResultOK) 
             {
-                UIManager.Instance.ClearAllPanel();
-                UIManager.Instance.ShowPanel<StartPanel>();
+                // k_EResultNoConnection - your Steam client doesn't have a connection to the back-end
+                // k_EResultTimeout - you the message to the Steam servers, but it didn't respond
+                // k_EResultFail - the server responded, but with an unknown internal error
+                // k_EResultAccessDenied - your game isn't set to allow lobbies, or your client does haven't rights to play the game
+                // k_EResultLimitExceeded - your game client has created too many lobbies
+                string EContent;
+                switch (o.m_eResult)
+                {
+                    case EResult.k_EResultNoConnection:
+                        EContent = "Steam未连接";
+                        break;
+                    case EResult.k_EResultTimeout:
+                        EContent = "Steam未响应";
+                        break;
+                    case EResult.k_EResultFail:
+                        EContent = "Steam相应出错";
+                        break;
+                    case EResult.k_EResultAccessDenied:
+                        EContent = "Steam不允许你在该游戏创建大厅";
+                        break;
+                    case EResult.k_EResultLimitExceeded:
+                        EContent = "你创建太多的大厅";
+                        break;
+                        default:
+                        EContent = o.m_eResult.ToString();
+                        break;
+                }
+                UIManager.Instance.ShowPanel<TipPanel>((p) => { p.SetCurrent("房间创建失败\n" + "错误原因：" + EContent ,true); });
             }
             else
             {
--- Assets/Scripts/UI/TipPanel.cs
+++ Assets/Scripts/UI/TipPanel.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class TipPanel : BasePanel
 {
@@ -9,5 +11,17 @@ public class TipPanel : BasePanel
         
     }
 
-    
+    public void SetCurrent(string content,bool ifShowButton = false,string btnName = "È·¶¨",UnityAction callback = null)
+    {
+        GetControl<Text>("txtContent").text = content;
+        if (ifShowButton)
+        {
+            GetControl<Text>("txtSure").text = btnName;
+            GetControl<Button>("btnSure").onClick.AddListener(() => 
+            {
+                UIManager.Instance.HidePanel<TipPanel>(); 
+                callback?.Invoke();
+            });
+        }
+    }
 }
